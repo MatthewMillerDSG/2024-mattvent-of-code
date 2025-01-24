826b15b2d5372bf3d82e72ba380472ace8c00058
Matthew <matthew.miller@dcsg.com>
2025-01-24 14:07:32 -0500

    mattventOfCode-01 | Matt | part 2

--- mattvent-of-code/day1/src/main/java/org/day1/Main.kt
+++ mattvent-of-code/day1/src/main/java/org/day1/Main.kt
@@ -8,15 +8,23 @@ fun main() {
     val left = mutableListOf<Int>()
     val right = mutableListOf<Int>()
 
-    // Part 1
+    // Part 1 - find the sum of the differences between sorted values
     input
         .lines()
         .map { line -> line.split("\\s+".toRegex())}
         .forEach{(l, r) -> left.add(l.toInt()); right.add(r.toInt())}
 
     println(left.sorted().zip(right.sorted()).sumOf { (l,r) -> abs(l-r) })
+    //my answer: 1258579   result: correct!
 
+    // Part 2 - find the number of occurrences in the right column of each number of the left
+    //            sum all the number of occurrences of each number by the value
+    val occurrences = right.groupingBy { it }.eachCount()
+
+    println(left.sumOf { l -> l * (occurrences[l] ?: 0)})
+    //my answer: 23981443    result: correct!
 }
+
 
 fun readInput(fileName: String): String {
     return ClassLoader.getSystemResource(fileName).readText()
