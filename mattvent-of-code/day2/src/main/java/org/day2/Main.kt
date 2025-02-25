cd3a1ae8b5fc8b3096520cda804babab9fd0b510
Matthew <matthew.miller@dcsg.com>
2025-02-25 15:49:42 -0500

    day2part1

---
+++ mattvent-of-code/day2/src/main/java/org/day2/Main.kt
@@ -0,0 +1,48 @@
+package org.day2
+
+import kotlin.math.abs
+
+fun main() {
+    val input = readInput("day2input.txt")
+//    val input = readInput("day2example.txt")
+    val lines = input.lines().map({ line -> line.split("\\s+".toRegex()) })
+    var count = 0
+    // Part 1 - find number of safe rows by:
+    //      * row values are all increasing or all decreasing
+    //      * adjacent values are different by at least 1 and at most 3
+    //answer: 383
+    lines.forEach({ line ->
+        if ((isDecreasing(line)) && smallDifference(line)) {
+            print(line)
+            println(" decreasing")
+            count++
+        } else if (isIncreasing(line) && smallDifference(line)) {
+            print(line)
+            println(" increasing")
+                count++
+            }
+        else
+            println(line + " not good")
+    })
+
+    println("number of increasing or decreasing: ${count}")
+}
+
+fun isIncreasing(line: List<String>): Boolean {
+    if (line.size == 1) return true
+    return line.windowed(2) { (l, r) -> l.toInt() < r.toInt() }.all { it }
+}
+
+fun isDecreasing(line: List<String>): Boolean {
+    if (line.size == 1) return true
+    return line.windowed(2) { (l, r) -> l.toInt() > r.toInt() }.all { it }
+}
+
+fun smallDifference(line: List<String>): Boolean {
+    if (line.size <= 1) return false
+    return line.windowed(2) { (l, r) -> abs(l.toInt() - r.toInt()) }.all { it in 1..3 }
+}
+
+fun readInput(fileName: String): String {
+    return ClassLoader.getSystemResource(fileName).readText()
+}
